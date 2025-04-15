10da5e6f67ed76a801ba0c8396dae2d3a3046b36
Hagemaruda <hagemaruda@gmail.com>
2025-04-15 14:02:05 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Azure test</h1>
+      <div>Skytech Hagiwara</div>
+    </>
+  )
+}
+
+export default App
